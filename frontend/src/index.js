5568b647b19434212c818a94ae13695eb917abbf
rgrouchy-avaya <rgrouchy@avaya.com>
2025-10-03 14:09:09 -0400

    adding /attendance to url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/attendance">
       <App />
     </BrowserRouter>
   </React.StrictMode>
